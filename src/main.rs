38ca9f2b4abefad0575c86443e96253d202a0fee
man2k <mankarmakar4@gmail.com>
2023-06-04 03:24:47 +0530

    variables, and identifiers, mut and unmut, stdin function and return types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// #![warn(non_snake_case)]
+// #![allow(unused)] // to ignore errors with unused variables
+// use std::io::*; For importing everything
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name");
+    //all variables are immutable by default
+    let mut name: String = String::new(); //function to create a empty string
+    let greeting: &str = "by man2K"; // immutable variable
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Didn't Receive input");
+    println!("Hello, {}! {}", name.trim_end(), greeting); // if ! is present then it is a macro
 }
